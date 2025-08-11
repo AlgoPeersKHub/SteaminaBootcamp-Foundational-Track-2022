0a755333f09f4770f7b1f7ae6cce176388a7b947
Algo Peers_KnowledgeHub <150908294+AlgoPeersKHub@users.noreply.github.com>
2025-08-11 22:37:18 +0000

    Updated Measuring Environmental Temperature.py

--- Micro:bit Temperature Sensor.py
+++ Micro:bit Temperature Sensor.py
@@ -1,40 +1,23 @@
 """
-
 ## Introduction
-
 Ever wondered how you could visualize the temperature using your micro:bit? 
-
 This project allows you to do just that! You'll explore the temperature sensor, buttons, and LEDs of the micro:bit.
 
-
-
 ## Parts of the Micro:bit
-
 1. **Temperature Sensor:** Measures and provides temperature data.
 2. **LEDs (Light Emitting Diode):** Displays temperature-related information.
 3. **Buttons:** Input for interaction with the micro:bit.
 
-
 ## Programming Concepts
+   Function:** A reusable block of code that performs a specific task.Exampe: Line 38
 
- **Function:** A reusable block of code that performs a specific task.Exampe: Line 38
-
-
-## Programming Language
-
-  Python
-
+## Programming Language: Python
 
 ## Coding Environment / Text Editor
-
-  [Microsoft MakeCode Editor](https://makecode.microbit.org/#editor)
-
+[Microsoft MakeCode Editor](https://makecode.microbit.org/#editor)
 
 **Feel free to comment or contribute**
-
 """
-
-
 
 # Project Code / Program ...
 

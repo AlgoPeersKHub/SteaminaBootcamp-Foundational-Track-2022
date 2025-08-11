c9b32b5099d166cc5eee6a89cb5cdcc49721b7cd
Algo Peers_KnowledgeHub <150908294+AlgoPeersKHub@users.noreply.github.com>
2025-08-11 19:56:01 +0000

    Update Flash How You Feel.py

--- Flash How You Feel.py
+++ Flash How You Feel.py
@@ -1,43 +1,33 @@
 """
 ## Introduction
-
 The objective of this project is to bring your emotions to life by creating animated emojis that flash on the micro:bit.
 
-
-Parts of the micro:bit used
-
+## Parts of the micro:bit used
   Accelerometer Sensor: Detects micro:bit shakes to express your emotions.
-   
   LEDs: Output that visually displays your emotions on the micro:bit.
 
 
-Programming concepts
+##Programming concepts
 
   Function:  A function is a reusable block of code that performs a specific task or a set of tasks. Example: Line 40
 
    Loop :  Loops help computers do things repeatedly or over and over again. Example Line 42
 
-          There are differnt types of loops the one used in the above program is called **for loop**. 
+           There are differnt types of loops the one used in the above program is called **for loop**. 
           
           **For Loop: It's like telling the micro:bit, "Display how I feel four(4) times.**
+          **While Loop: It’s like telling the micro:bit, "Keep showing how I feel while button A is pressed."**
 
 
-Programming Language
-       Python
+## Programming Language: Python
 
-
-Text Editor / Coding Environment
+## Text Editor / Coding Environment
       [Microsoft Makecode for Micro:bit](https://makecode.microbit.org/#editor)
-
 
 ** Feel free to comment or contribute**
 """
 
-
-
 # Project Code / Program ...
-
-
 def on_gesture_shake():  # Display a sequence of icons when the micro:bit is shaken
     
     for index in range(4):
@@ -52,6 +42,19 @@ def on_gesture_shake():  # Display a sequence of icons when the micro:bit is sha
 input.on_gesture(Gesture.SHAKE, on_gesture_shake)  # Set up an event handler for the SHAKE gesture
 
 
+def on_button_pressed_a():
+    while True:
+        basic.show_icon(IconNames.HEART)
+        # Show a surprised face
+        basic.pause(5000)
+        basic.show_icon(IconNames.CONFUSED)
+        # Show a surprised face
+        basic.pause(5000)
+        basic.show_icon(IconNames.SAD)
+        # Show a surprised face
+        basic.pause(5000)
+        basic.show_icon(IconNames.ASLEEP)
+input.on_button_pressed(Button.A, on_button_pressed_a)
 
 
 

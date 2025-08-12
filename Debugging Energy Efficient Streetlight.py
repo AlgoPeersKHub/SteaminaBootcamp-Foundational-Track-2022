b33b95531a4a635b1b79926ddf2dbe7c8d39f561
Algo Peers_KnowledgeHub <150908294+AlgoPeersKHub@users.noreply.github.com>
2025-08-12 19:32:09 +0000

    Update Debugging Energy Efficient Streetlight.py

--- Debugging Energy Efficient Streetlight.py
+++ Debugging Energy Efficient Streetlight.py
@@ -1,7 +1,11 @@
 ## The provided energy-efficient streetlight program contains errors. 
 ## Your task is to identify, analyze, and correct these bugs to ensure the energy-efficient streetlight operates as intended
 
+##Feel free to comment, contribute, or use the code for your project.
+#You can use the same pins or change them—just be sure to update the code accordingly so your project works correctly."""
 
+
+## Project code / program
 def on_forever():
     if input.light_level() > 50:
         basic.show_leds("""

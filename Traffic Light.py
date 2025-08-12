2c1dfc6ee0096d91ed2af9beecc6321be1cc82d9
Algo Peers_KnowledgeHub <150908294+AlgoPeersKHub@users.noreply.github.com>
2025-08-12 11:26:22 +0000

    Updated Traffic Light.py

--- Traffic Light.py
+++ Traffic Light.py
@@ -16,22 +16,26 @@ Please note that the timer for light changes in this simulation does not adhere 
 ## Coding Environment / Text Editor
 ##[Microsoft MakeCode Editor](https://makecode.microbit.org/#editor)
 
-## Feel free to comment or contribute"""
+##Feel free to comment, contribute, or use the code for your project.
+You can use the same pins or change them—just be sure to update the code accordingly so your project works correctly."""
 
 
 # Project Code / Program ...
 def on_forever(): # Define a function called on_forever
     pins.digital_write_pin(DigitalPin.P0, 1)   # Turn on the red light (P0)
+    basic.show_string("Stop!")
     basic.pause(5000)  # Pause for 5 seconds
     basic.pause(5000)    # Pause for an additional 5 seconds
     pins.digital_write_pin(DigitalPin.P0, 0)  # Turn off the red light (P0)
 
     pins.digital_write_pin(DigitalPin.P1, 1)   # Turn on the amber light (P1)
+    basic.show_string("Get Ready!")
     basic.pause(5000)
     basic.pause(5000)
     pins.digital_write_pin(DigitalPin.P1, 0) # Turn off the amber light (P1)
 
     pins.digital_write_pin(DigitalPin.P2, 1)  # Turn on the green light (P2)
+    basic.show_string("Move!")
     basic.pause(5000)
     basic.pause(5000)
     pins.digital_write_pin(DigitalPin.P2, 0) # Turn off the green light (P2)
